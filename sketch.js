174225638a524ae358adef9dc5a3921aa1d06dec
Will Luong <tluong@stedwards.edu>
2020-06-17 22:03:33 -0500

    Adding the startPoint and endPoint to a list

--- sketch.js
+++ sketch.js
@@ -27,13 +27,17 @@ function removeFromArray(arr, ele)
 
 function getFormData()
 {
+    var coordinate = [];
     var startPoint = document.getElementById("startPoint").value;
     var endPoint = document.getElementById("endPoint").value;
-    console.log("Start Point: ");
-    console.log(startPoint);
-    console.log("End Point: ");
-    console.log(endPoint);
+
+    coordinate.push(startPoint);
+    coordinate.push(endPoint);
+    return coordinate;
+    
 }
+
+
 function heuristic(a,b)
 {
     //calculating the distance between the two node a and b
